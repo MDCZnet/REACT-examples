e34336f359e0f542ca5a2e6a9fda6f027c0e00dc
Martin Dittrich <9496368+MDCZnet@users.noreply.github.com>
2024-03-22 13:00:03 +0100

    Update

---
+++ react-icons/App.js
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+export default function App() {
+  return (
+    <Text>
+      <Icon name="home" size={30} color="#900" />;
+    </Text>
+  );
+}
