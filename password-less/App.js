ebfdb24a2ce084b068d112640ef0373fcf0bcf9c
Martin Dittrich <9496368+MDCZnet@users.noreply.github.com>
2024-05-15 14:55:22 +0200

    Update

--- password-less/App.js
+++ password-less/App.js
@@ -4,10 +4,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { theme } from './src/core/theme'
 import {
-  StartScreen,
   LoginScreen,
-  RegisterScreen,
-  ResetPasswordScreen,
   Dashboard,
 } from './src/screens'
 
@@ -18,19 +15,13 @@ export default function App() {
     <Provider theme={theme}>
       <NavigationContainer>
         <Stack.Navigator
-          initialRouteName="StartScreen"
+          initialRouteName="LoginScreen"
           screenOptions={{
             headerShown: false,
           }}
         >
-          <Stack.Screen name="StartScreen" component={StartScreen} />
           <Stack.Screen name="LoginScreen" component={LoginScreen} />
-          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
           <Stack.Screen name="Dashboard" component={Dashboard} />
-          <Stack.Screen
-            name="ResetPasswordScreen"
-            component={ResetPasswordScreen}
-          />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
--- password-less/src/screens/Dashboard.js
+++ password-less/src/screens/Dashboard.js
@@ -1,24 +1,21 @@
 import React from 'react'
 import Background from '../components/Background'
-import Logo from '../components/Logo'
-import Header from '../components/Header'
+
 import Paragraph from '../components/Paragraph'
 import Button from '../components/Button'
 
 export default function Dashboard({ navigation }) {
   return (
     <Background>
-      <Logo />
-      <Header>Welcome 💫</Header>
       <Paragraph>
-        Congratulations you are logged in.
+        Přihlášení do aplikace proběhlo úspěšně.
       </Paragraph>
       <Button
         mode="outlined"
         onPress={() =>
           navigation.reset({
             index: 0,
-            routes: [{ name: 'StartScreen' }],
+            routes: [{ name: 'LoginScreen' }],
           })
         }
       >
